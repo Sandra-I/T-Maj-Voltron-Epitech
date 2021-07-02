0355771972a0924fb0db51f94c793ba492390b3f
S-I <sandra.ibrahim@hotmail.fr>
2021-07-02 16:58:07 +0200

    [ADD] Méthode pour déterminer la santé de la feuille

--- src/views/Ia.vue
+++ src/views/Ia.vue
@@ -9,18 +9,35 @@ const axios = require('axios');
 
 export default {
   name: 'Ia',
+  data () {
+    return {
+      // leafImage: '',
+      lastImageIdProcess: ''
+    }
+  },
   methods: {
     test () {
       const baseURL = 'http://api-voltron.herokuapp.com/api/';
       axios.get(`${baseURL}/images_process/lasted`)
       .then(response => {
-        console.log(response.data);
-        var image = new Image();
-        image.src = `data:image/png;base64,${response.data.image}`;
+        console.log(response.data.status);
+        // const base6 = response.data.image.base_64;
+        const healthStatus = response.data.status;
+
+        this.isItSick(healthStatus)
+        // var image = new Image();
+        // image.src = `data:image/png;base64,${response.data.image}`;
       })
       .catch(error => {
         console.log(error.message)
       })
+    },
+    isItSick (status) {
+      if (status < 0.5) {
+        console.log('status < 0.5 pas malade')
+      } else {
+        console.log('status > 0.5 malade')
+      }
     }
   },
   mounted () {
