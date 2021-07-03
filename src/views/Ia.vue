d0675a4e8bfaed13922f3afde97b4b6a99fb12e2
S-I <sandra.ibrahim@hotmail.fr>
2021-07-03 12:51:14 +0200

    [UP] un début de récupération des infos utilisateurs

--- src/views/Ia.vue
+++ src/views/Ia.vue
@@ -2,6 +2,7 @@
   <div class="card">
     <h1 class="card__title">Santé des vignes</h1>
     <p class="card__subtitle">Malade ou pas malade ?</p>
+    <p>{{user.firstname}} {{user.lastname}} {{user.email}} {{user.id}}</p>
     <div class="card__subtitle">
       <img class="img" v-if="isSick" :src="'data:image/jpeg;base64,'+ base64"/>
     </div>
@@ -12,11 +13,13 @@
       <h3 v-else>
         Pas de maladie repérée sur les vignes !
       </h3>
+      <button @click="checkStore()" class="button">Store</button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import axios from 'axios';
 import emailjs from 'emailjs-com';
 
@@ -29,6 +32,11 @@ export default {
       isSick: false
     }
   },
+  computed: {
+    ...mapState({
+      user: 'userInfos',
+    })
+  },
   methods: {
     getLastImageProcess () {
       const baseURL = 'http://api-voltron.herokuapp.com/api/';
@@ -40,7 +48,7 @@ export default {
           this.base64 = response.data.image.base_64;
           this.lastImageIdProcess = response.data.image._id;
         } else {
-          console.log('Pas de nouvelles images')
+          console.log('Pas de nouvelles images');
         }
       })
       .catch(error => {
@@ -57,8 +65,8 @@ export default {
     },
     sendEmailAlert () {
       var templateParams = {
-        to_email: 'sandra.ibrahim@hotmail.fr',
-        to_name: 'James'
+        to_email: this.user.email,
+        to_name: this.user.firstname
       };
       emailjs.init("user_MfUUTFhCzCsBRtj45OCUH");
       emailjs.send('service_mbfftpo', 'template_alerte', templateParams)
@@ -71,6 +79,9 @@ export default {
     setIntervalGetLastImageProcess () {
       // 300000 millisecondes = 5 minutes
       setInterval (this.getLastImageProcess, 300000);
+    }, 
+    checkStore () {
+      console.log('this.user', this.user)
     }
   },
   mounted () {
